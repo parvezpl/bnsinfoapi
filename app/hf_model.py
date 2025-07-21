9fcadf6aedc40a8864b7ed592a38120526fe7850
parvezpl <parvezalam336@gmail.com>
2025-07-21 22:02:09 +0530

    hello

--- app/hf_model.py
+++ app/hf_model.py
@@ -1,8 +1,11 @@
 from sentence_transformers import SentenceTransformer
 
 # Load from Hugging Face Hub (model not stored locally)
-model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+# model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 
+def get_model():
+    return SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 
 def get_embedding(text: str):
+    model = get_model()
     return model.encode(text).tolist()
